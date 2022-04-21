e74ad3dd6766692ba5efb6dfa8c9b89c4746b795
Braden MacDonald <braden@opencraft.com>
2022-04-21 16:12:16 +0100

    Initial commit from cookiecutter

---
+++ README.rst
@@ -0,0 +1,25 @@
+arm64 plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+This is a simple plugin that makes it as easy as possible to get started using
+Tutor on ARM64 systems.
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/open-craft/tutor-contrib-arm64
+
+Usage
+-----
+
+::
+
+    tutor plugins enable arm64
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,59 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutor_arm64", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="tutor-contrib-arm64",
+    version=ABOUT["__version__"],
+    url="https://github.com/open-craft/tutor-contrib-arm64",
+    project_urls={
+        "Code": "https://github.com/open-craft/tutor-contrib-arm64",
+        "Issue tracker": "https://github.com/open-craft/tutor-contrib-arm64/issues",
+    },
+    license="AGPLv3",
+    author="Braden MacDonald",
+    description="arm64 plugin for Tutor",
+    long_description=load_readme(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["tutor"],
+    entry_points={
+        "tutor.plugin.v1": [
+            "arm64 = tutor_arm64.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
